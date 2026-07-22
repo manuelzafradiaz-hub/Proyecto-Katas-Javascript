9ecbb77c8a94354a44260f41eb15fb511b52990a
Manuel Zafra <manuelzafradiaz@gmail.com>
2026-07-22 21:55:32 +0200

    8 proyectos

---
+++ ejercicio-07.js
@@ -0,0 +1,10 @@
+greaterNumber(78, 65);   
+greaterNumber(18, 29); 
+function greaterNumber(numberOne , numberTwo) {
+  // Completar código
+    if (numberOne > numberTwo) {
+        console.log("El número más alto es:", numberOne);
+    } else {
+        console.log("El número más alto es:", numberTwo);
+    }
+}
---
+++ ejercicio-08.js
@@ -0,0 +1,24 @@
+const avengers = [
+  "Hulk",
+  "Thor",
+  "Iron Man",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(stringList) {
+  // Completar código
+
+ let longestWord= "";
+ for (let i = 0; i < stringList.length; i++) {
+    if (stringList[i].length > longestWord.length) {
+        longestWord = stringList[i];
+     }
+  }
+ return longestWord;
+
+}
+console.log(findLongestWord(avengers));
+
+
+
