2a15a91bc9b18393c6b1251443368b4d2bf3f1ed
manuelzafradiaz-hub <manuelzafradiaz@gmail.com>
2026-07-23 20:42:11 +0200

    Crear carpeta ejercicios-basicos-avanzados y organizar ejercicios

---
+++ ejercicios-basicos-avanzados/ejercicio-03.js
@@ -0,0 +1,22 @@
+/* 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+3.2 Divide 10 por 2 y muestra el resultado en un console.
+3.3 Muestra mediante un console el resto de dividir 15 por 9.
+3.4 Usa el correcto operador de asignación que resultará en o = 15,
+teniendo dos variables p = 10 y j = 5.
+3.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5. /*/
+
+const multiplicacion = 10 * 5;
+console.log(multiplicacion);
+const division = 10 / 2;
+console.log(division);
+const resto = 15 % 9;
+console.log(resto);
+const p = 10;
+const j = 5;
+let o = p + j;
+console.log(o);
+const c = 10;
+const m = 5;
+let i = c * m;
+console.log(i);
