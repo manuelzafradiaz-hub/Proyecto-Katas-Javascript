1f3c1450d4817a2ef50c6bdf7f18da799bbc2817
Manuel Zafra <manuelzafradiaz@gmail.com>
2026-07-31 14:13:46 +0200

    25 proyectos

--- ejercicio-25.js
+++ ejercicio-25.js
@@ -1,7 +1,3 @@
-/*Dada una lista de películas, cuenta cuántas son de antes del año 2000 y cuántas son posteriores, utilizando un bucle.
-
-Imprime ambos conteos por consola./*/
-
 const movies = [
   { title: 'The Matrix', releaseYear: 1999 },
   { title: 'Star Wars: Episode IV – A New Hope', releaseYear: 1977 },
